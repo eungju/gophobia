a7864ee1dfd1845e9e941c3cf088866a352023ea
Park Eungju <eungju@gmail.com>
2017-01-13 16:13:31 +0900

    Parse unified commands.

--- resp/resp.go
+++ resp/resp.go
@@ -4,32 +4,86 @@ import (
 	"bufio"
 	"io"
 	"regexp"
+	"strconv"
+	"fmt"
 )
 
 const (
-	ErrorReply  = '-'
-	StatusReply = '+'
-	IntReply    = ':'
-	StringReply = '$'
-	ArrayReply  = '*'
+	simpleStringType = '+'
+	errorType  = '-'
+	intType    = ':'
+	bulkStringType = '$'
+	arrayType  = '*'
 )
 
-type Reader struct {
+var lineSeparator = []byte("\r\n")
+var lineSeparatorBytes = len(lineSeparator)
+
+type RequestReader struct {
 	source *bufio.Reader
 }
 
-func NewReader(source io.Reader) *Reader {
-	return &Reader{source: bufio.NewReader(source)}
+func NewRequestReader(source io.Reader) *RequestReader {
+	return &RequestReader{source: bufio.NewReader(source)}
 }
 
-func (self *Reader) Read() ([]string, error) {
-	line, isPrefix, err := self.source.ReadLine()
+func (reader *RequestReader) ReadLine() (string, error) {
+	line, isPrefix, err := reader.source.ReadLine()
+	if err != nil {
+		return "", err
+	}
+	if isPrefix {
+		return "", bufio.ErrBufferFull
+	}
+	return string(line), nil
+}
+
+func (reader *RequestReader) Read() ([]string, error) {
+	line, err := reader.ReadLine()
 	if err != nil {
 		return nil, err
 	}
-	if isPrefix {
-		return nil, bufio.ErrBufferFull
+	if line[0] == arrayType {
+		n, err := strconv.Atoi(line[1:])
+		if err != nil {
+			return nil, err
+		}
+		array := make([]string, n)
+		for i := 0; i < n; i++ {
+			element, err := reader.ReadBulkString()
+			if err != nil {
+				return nil, err
+			}
+			array[i] = element
+		}
+		return array, nil
 	}
 	inlineCommandSeparator := regexp.MustCompile("\\s+")
 	return inlineCommandSeparator.Split(string(line), -1), nil
+}
+
+func (reader *RequestReader) ReadBulkString() (string, error) {
+	line, err := reader.ReadLine()
+	if err != nil {
+		return "", err
+	}
+	respType := line[0]
+	if line[0] != bulkStringType {
+		return "", fmt.Errorf("Expected bulk string type but %s", respType)
+	}
+	bytes, err := strconv.Atoi(line[1:])
+	if err != nil {
+		return "", err
+	}
+	buf := make([]byte, bytes)
+	readBytes, err := reader.source.Read(buf)
+	if bytes != readBytes {
+		return "", fmt.Errorf("Expected %d bytes but %d", bytes, readBytes)
+	}
+	discardBuf := make([]byte, 2)
+	discardBytes, err := reader.source.Read(discardBuf)
+	if lineSeparatorBytes != discardBytes {
+		return "", fmt.Errorf("Expected CRLF but %s", discardBuf)
+	}
+	return string(buf), nil
 }
--- resp/resp_test.go
+++ resp/resp_test.go
@@ -8,10 +8,15 @@ import (
 )
 
 func TestParsingInlineCommand(t *testing.T) {
-	dut := NewReader(strings.NewReader("mget a b c\r\n"))
+	dut := NewRequestReader(strings.NewReader("mget a b c\r\n"))
 	r, err := dut.Read()
-	if err != nil {
-		t.Error(err)
-	}
+	assert.NoError(t, err)
+	assert.Equal(t, []string{"mget", "a", "b", "c"}, r)
+}
+
+func TestParsingUnifiedCommand(t *testing.T) {
+	dut := NewRequestReader(strings.NewReader("*4\r\n$4\r\nmget\r\n$1\r\na\r\n$1\r\nb\r\n$1\r\nc\r\n"))
+	r, err := dut.Read()
+	assert.NoError(t, err)
 	assert.Equal(t, []string{"mget", "a", "b", "c"}, r)
 }
