9cbacbae5010c3adb6ae22516882570861d36c6e
Park Eungju <eungju@gmail.com>
2017-01-13 22:34:32 +0900

    RESP data types.

--- resp/resp.go
+++ resp/resp.go
@@ -18,72 +18,198 @@ const (
 
 var lineSeparator = []byte("\r\n")
 var lineSeparatorBytes = len(lineSeparator)
+var inlineCommandSeparator = regexp.MustCompile("\\s+")
 
-type RequestReader struct {
+type RespValue interface {
+	AsSimpleString() (*RespSimpleString, error)
+	AsError() (*RespError, error)
+	AsBulkString() (*RespBulkString, error)
+	AsArray() (*RespArray, error)
+}
+
+type RespSimpleString struct {
+	Value []byte
+}
+
+func (resp *RespSimpleString) AsSimpleString() (*RespSimpleString, error) {
+	return resp, nil
+}
+
+func (resp *RespSimpleString) AsError() (*RespError, error) {
+	return nil, fmt.Errorf("Expected RespError but RespSimpleString")
+}
+
+func (resp *RespSimpleString) AsBulkString() (*RespBulkString, error) {
+	return nil, fmt.Errorf("Expected RespBulkString but RespSimpleString")
+}
+
+func (resp *RespSimpleString) AsArray() (*RespArray, error) {
+	return nil, fmt.Errorf("Expected RespArray but RespSimpleString")
+}
+
+type RespError struct {
+	Value []byte
+}
+
+func (resp *RespError) AsSimpleString() (*RespSimpleString, error) {
+	return nil, fmt.Errorf("Expected RespSimpleString but RespError")
+}
+
+func (resp *RespError) AsError() (*RespError, error) {
+	return resp, nil
+}
+
+func (resp *RespError) AsBulkString() (*RespBulkString, error) {
+	return nil, fmt.Errorf("Expected RespBulkString but RespError")
+}
+
+func (resp *RespError) AsArray() (*RespArray, error) {
+	return nil, fmt.Errorf("Expected RespArray but RespError")
+}
+
+
+type RespBulkString struct {
+	Value []byte
+}
+
+func (resp *RespBulkString) AsSimpleString() (*RespSimpleString, error) {
+	return nil, fmt.Errorf("Expected RespSimpleString but RespBulkString")
+}
+
+func (resp *RespBulkString) AsError() (*RespError, error) {
+	return nil, fmt.Errorf("Expected RespError but RespBulkString")
+}
+
+func (resp *RespBulkString) AsBulkString() (*RespBulkString, error) {
+	return resp, nil
+}
+
+func (resp *RespBulkString) AsArray() (*RespArray, error) {
+	return nil, fmt.Errorf("Expected RespArray but RespBulkString")
+}
+
+type RespArray struct {
+	Value []RespValue
+}
+
+func (resp *RespArray) AsSimpleString() (*RespSimpleString, error) {
+	return nil, fmt.Errorf("Expected RespSimpleString but RespArray")
+}
+
+func (resp *RespArray) AsError() (*RespError, error) {
+	return nil, fmt.Errorf("Expected RespError but RespArray")
+}
+
+func (resp *RespArray) AsBulkString() (*RespBulkString, error) {
+	return nil, fmt.Errorf("Expected RespBulkString but RespArray")
+}
+
+func (resp *RespArray) AsArray() (*RespArray, error) {
+	return resp, nil
+}
+
+type Reader struct {
 	source *bufio.Reader
 }
 
-func NewRequestReader(source io.Reader) *RequestReader {
-	return &RequestReader{source: bufio.NewReader(source)}
+func NewReader(source io.Reader) *Reader {
+	return &Reader{source: bufio.NewReader(source)}
 }
 
-func (reader *RequestReader) ReadLine() (string, error) {
-	line, isPrefix, err := reader.source.ReadLine()
-	if err != nil {
-		return "", err
-	}
-	if isPrefix {
-		return "", bufio.ErrBufferFull
-	}
-	return string(line), nil
-}
-
-func (reader *RequestReader) Read() ([]string, error) {
-	line, err := reader.ReadLine()
+func (reader *Reader) Read() (RespValue, error) {
+	line, err := reader.readLine()
 	if err != nil {
 		return nil, err
 	}
-	if line[0] == arrayType {
-		n, err := strconv.Atoi(line[1:])
+	respType := line[0]
+	if simpleStringType == respType {
+		return &RespSimpleString{line[1:]}, nil
+	} else if errorType == respType {
+		return &RespError{line[1:]}, nil
+	} else if arrayType == respType {
+		length, err := strconv.Atoi(string(line[1:]))
 		if err != nil {
 			return nil, err
 		}
-		array := make([]string, n)
-		for i := 0; i < n; i++ {
-			element, err := reader.ReadBulkString()
-			if err != nil {
-				return nil, err
-			}
-			array[i] = element
+		return reader.readArray(length)
+	} else if bulkStringType == respType {
+		bytes, err := strconv.Atoi(string(line[1:]))
+		if err != nil {
+			return nil, err
 		}
-		return array, nil
+		return reader.readBulkString(bytes)
 	}
-	inlineCommandSeparator := regexp.MustCompile("\\s+")
-	return inlineCommandSeparator.Split(string(line), -1), nil
+	return nil, fmt.Errorf("Unknown type %s", respType)
 }
 
-func (reader *RequestReader) ReadBulkString() (string, error) {
-	line, err := reader.ReadLine()
+func (reader *Reader) readBulkString(bytes int) (*RespBulkString, error) {
+	value := make([]byte, bytes)
+	readBytes, err := reader.source.Read(value)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	respType := line[0]
-	if line[0] != bulkStringType {
-		return "", fmt.Errorf("Expected bulk string type but %s", respType)
-	}
-	bytes, err := strconv.Atoi(line[1:])
-	if err != nil {
-		return "", err
-	}
-	buf := make([]byte, bytes)
-	readBytes, err := reader.source.Read(buf)
 	if bytes != readBytes {
-		return "", fmt.Errorf("Expected %d bytes but %d", bytes, readBytes)
+		return nil, fmt.Errorf("Expected %d bytes but %d", bytes, readBytes)
 	}
 	discardBuf := make([]byte, 2)
 	discardBytes, err := reader.source.Read(discardBuf)
 	if lineSeparatorBytes != discardBytes {
-		return "", fmt.Errorf("Expected CRLF but %s", discardBuf)
+		return nil, fmt.Errorf("Expected CRLF but %s", discardBuf)
 	}
-	return string(buf), nil
+	return &RespBulkString{value}, nil
+}
+
+func (reader *Reader) readArray(length int) (*RespArray, error) {
+	value := make([]RespValue, length)
+	for i := 0; i < length; i++ {
+		element, err := reader.Read()
+		if err != nil {
+			return nil, err
+		}
+		value[i] = element
+	}
+	return &RespArray{value}, nil
+}
+
+func (reader *Reader) readLine() ([]byte, error) {
+	line, isPrefix, err := reader.source.ReadLine()
+	if err != nil {
+		return nil, err
+	}
+	if isPrefix {
+		return nil, bufio.ErrBufferFull
+	}
+	return line, nil
+}
+
+type CommandReader struct {
+	Reader
+}
+
+func NewCommandReader(source io.Reader) *CommandReader {
+	return &CommandReader{Reader{bufio.NewReader(source)}}
+}
+
+func (reader *CommandReader) Read() ([]string, error) {
+	line, err := reader.readLine()
+	if err != nil {
+		return nil, err
+	}
+	if line[0] == arrayType {
+		length, err := strconv.Atoi(string(line[1:]))
+		if err != nil {
+			return nil, err
+		}
+		array, err := reader.readArray(length)
+		command := make([]string, length)
+		for i := 0; i < length; i++ {
+			bs, err := array.Value[i].AsBulkString()
+			if err != nil {
+				return nil, err
+			}
+			command[i] = string(bs.Value)
+		}
+		return command, nil
+	}
+	return inlineCommandSeparator.Split(string(line), -1), nil
 }
--- resp/resp_test.go
+++ resp/resp_test.go
@@ -7,16 +7,38 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestParsingSimpleString(t *testing.T) {
+	dut := NewReader(strings.NewReader("+OK\r\n"))
+	actual, _ := dut.Read()
+	assert.Equal(t, &RespSimpleString{[]byte("OK")}, actual)
+}
+
+func TestParsingError(t *testing.T) {
+	dut := NewReader(strings.NewReader("-Error message\r\n"))
+	actual, _ := dut.Read()
+	assert.Equal(t, &RespError{[]byte("Error message")}, actual)
+}
+
+func TestParsingBulkString(t *testing.T) {
+	dut := NewReader(strings.NewReader("$3\r\nget\r\n"))
+	actual, _ := dut.Read()
+	assert.Equal(t, &RespBulkString{[]byte("get")}, actual)
+}
+
+func TestParsingArray(t *testing.T) {
+	dut := NewReader(strings.NewReader("*2\r\n$1\r\na\r\n$1\r\nb\r\n"))
+	actual, _ := dut.Read()
+	assert.Equal(t, &RespArray{[]RespValue{&RespBulkString{[]byte("a")}, &RespBulkString{[]byte("b")}}}, actual)
+}
+
 func TestParsingInlineCommand(t *testing.T) {
-	dut := NewRequestReader(strings.NewReader("mget a b c\r\n"))
-	r, err := dut.Read()
-	assert.NoError(t, err)
+	dut := NewCommandReader(strings.NewReader("mget a b c\r\n"))
+	r, _ := dut.Read()
 	assert.Equal(t, []string{"mget", "a", "b", "c"}, r)
 }
 
 func TestParsingUnifiedCommand(t *testing.T) {
-	dut := NewRequestReader(strings.NewReader("*4\r\n$4\r\nmget\r\n$1\r\na\r\n$1\r\nb\r\n$1\r\nc\r\n"))
-	r, err := dut.Read()
-	assert.NoError(t, err)
+	dut := NewCommandReader(strings.NewReader("*4\r\n$4\r\nmget\r\n$1\r\na\r\n$1\r\nb\r\n$1\r\nc\r\n"))
+	r, _ := dut.Read()
 	assert.Equal(t, []string{"mget", "a", "b", "c"}, r)
 }
